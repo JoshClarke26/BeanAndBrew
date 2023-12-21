c9fb14bf73e77eb2db8ae32206f4d351ff318177
JoshClarke26 <867589@student.derby-college.ac.uk>
2023-12-21 12:48:01 +0000

    Added home page and seed data

---
+++ BeanAndBrewV2/Data/Migrations/20231221115007_seedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeanAndBrewV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Espresso", 3.20, 300, "03236d8e-a387-4f4f-8e43-65a922021705.png" });
+
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Cappuccino", 3.50, 300, "07ef0b54-1d9c-4402-9222-820b3a6d5fee.png" });
+
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Mocha", 3.50, 300, "56c4eb85-3a51-4bd4-9227-dd777e31699b.png" });
+
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Latte", 3.50, 420, "6c7f263a-6c93-427d-9b93-1f8f78ebb55e.png" });
+
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Americano", 3.50, 330, "b4433f21-0d10-4ef1-bfda-59541ba43bcd.png" });
+
+            migrationBuilder.InsertData(
+                table: "Coffee",
+                columns: new[] { "Name", "Price", "Size", "ImagePath" },
+                values: new object[] { "Flat White", 3.50, 330, "c7049eff-e0c1-4f86-a625-b2ba309c26cb.png" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BeanAndBrewV2/Data/Migrations/20231221121121_seedData2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeanAndBrewV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "All Day Breakfast Wrap", 3.50, "Ult_Breakfast_Wrap_Thumb.jpg" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Bacon Bap", 2.80, "British_Bacon_Bap_Thumb.jpg" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Suasage Bap", 2.80, "Pork_Sausage_Bap_Thumb.jpg" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Croissant", 2.50, "New_All_Butter_Crossiant_Thumb.jpg" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Toast", 1.95, "Value_Offer_White_Toast_Thumb.jpg" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Orange Cake", 3.00, "Orange_Cake.png" });
+            migrationBuilder.InsertData(
+                table: "HamperItem",
+                columns: new[] { "Name", "Price", "ImagePath" },
+                values: new object[] { "Christmas Shortcake", 2.60, "Christmas_Tree_Shortcake_Thumb.jpg" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
